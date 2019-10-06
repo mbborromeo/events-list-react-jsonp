1917b10b57070c2ee0d776d8980ef2d80513bec3
Michael Borromeo <mbborromeo@gmail.com>
2019-10-05 21:11:12 -0700

    edit Event Details content order

--- src/Event.js
+++ src/Event.js
@@ -53,80 +53,69 @@ class Event extends React.Component {
     }
 
     render() {
-        //console.log("this.state.event is: ", this.state.event);
+        console.log("this.state.event is: ", this.state.event);
 
         return (
             <div>
                 { this.state.loading ?
                     <div>Loading event</div> :
-                    <div>
-                        { this.state.event.name &&
-                            <h1>
-                                { this.state.event.name }                                
-                            </h1>
-                        }
-
-                        { this.state.event.description &&
-                            <div>
-                                Description: { this.state.event.description }<br />
-                            </div>
-                        }
+                    <div>                        
+                        <h1>
+                            { this.state.event.name ? this.state.event.name : "" }                                
+                        </h1>
                         
-                        { this.state.event.categories && this.state.event.categories.length>0 &&
-                            <div>
-                                Categories: 
-                                {
-                                    this.state.event.categories.map(
-                                        (category) => <span key={ category.id }>{ category.name + ' ' }</span>
-                                    )
-                                }
-                            </div>
-                        }
+                        <div>
+                            Location: 
+                            { (this.state.event.location && this.state.event.location.name) ?
+                                ' ' + this.state.event.location.name
+                                : ""
+                            }
+                        </div>
+                                               
+                        <div>
+                            From: 
+                            { this.state.event.time_start ?
+                                ' ' + new Date(this.state.event.time_start).getDate() + '-' + 
+                                (new Date(this.state.event.time_start).getMonth()+1) + '-' + 
+                                new Date(this.state.event.time_start).getFullYear() + ', ' + 
+                                new Date(this.state.event.time_start).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' ' 
+                                : ""
+                            }
+                        </div>
 
-                        { this.state.event.time_start &&
-                            <div>
-                                From: 
-                                { 
-                                    ' ' + new Date(this.state.event.time_start).getDate() + '-' + 
-                                    (new Date(this.state.event.time_start).getMonth()+1) + '-' + 
-                                    new Date(this.state.event.time_start).getFullYear() + ', ' + 
-                                    new Date(this.state.event.time_start).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' ' 
-                                 }
-                                <br />
-                            </div>
-                        }
+                        <div>
+                            To: 
+                            { this.state.event.time_stop ?
+                                ' ' + new Date(this.state.event.time_stop).getDate() + '-' + 
+                                (new Date(this.state.event.time_stop).getMonth()+1) + '-' + 
+                                new Date(this.state.event.time_stop).getFullYear() + ', ' + 
+                                new Date(this.state.event.time_stop).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' '
+                                : ""
+                            }
+                        </div>
 
-                        { this.state.event.time_stop &&
-                            <div>
-                                To: 
-                                { 
-                                    ' ' + new Date(this.state.event.time_stop).getDate() + '-' + 
-                                    (new Date(this.state.event.time_stop).getMonth()+1) + '-' + 
-                                    new Date(this.state.event.time_stop).getFullYear() + ', ' + 
-                                    new Date(this.state.event.time_stop).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' '
-                                }
-                                <br />
-                            </div>
-                        }
+                        <div>
+                            Categories:&nbsp;
+                            { (this.state.event.categories && this.state.event.categories.length>0 ) ? 
+                                this.state.event.categories.map(
+                                    (category) => <span key={ category.id }>{ category.name + ' ' }</span>
+                                )
+                                : ""
+                            }
+                        </div>
 
-                        { this.state.event.location && this.state.event.location.name &&
-                            <div>
-                                Location: { this.state.event.location.name }<br />
-                            </div>
-                        }
-
+                        <div>
+                            Description: { this.state.event.description ? this.state.event.description : "" }
+                        </div>
                         
-                        { this.state.event.id &&
-                            <div>
-                                Event ID: { this.state.event.id }
-                            </div>
-                        }
-                        
-                        { this.state.event.thumbnail_image_url &&
-                            <div>
-                                <img src={ this.state.event.thumbnail_image_url } />
-                            </div>
-                        }
+                        <div>
+                            <img src={ this.state.event.thumbnail_image_url ? this.state.event.thumbnail_image_url : "" } />
+                        </div>
+
+                        <div>
+                            Event ID: 
+                            { this.state.event.id ? ' ' + this.state.event.id : "" }
+                        </div>
 
                         <br /><br />
 
