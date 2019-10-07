1c7a730c06c9b7e5f8385502cbbaba945508d94b
Michael Borromeo <mbborromeo@gmail.com>
2019-10-06 17:29:36 -0700

    refactor DateTimeFromTo

---
+++ src/DateTimeFromTo.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import * as Constants from './constants';
+
+class DateTimeFromTo extends React.Component {    
+    constructor(props) {
+        super(props);
+        
+        this.getMonthName = this.getMonthName.bind(this);
+    }
+
+    getMonthName(m) {
+        return Constants.MONTH_NAME[m];
+    }
+
+    render () {
+        return (
+            <div>
+                { this.props.time_start &&  
+                    <span>
+                        {
+                            new Date(this.props.time_start).getDate() + ' ' + 
+                            this.getMonthName( new Date(this.props.time_start).getMonth() ) + ' ' + 
+                            new Date(this.props.time_start).getFullYear() + ', ' + 
+                            new Date(this.props.time_start).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' - ' 
+                        }
+                    </span>
+                }
+
+                { this.props.time_stop && 
+                    <span>
+                        { (new Date(this.props.time_stop)!==new Date(this.props.time_start) && 
+                            new Date(this.props.time_stop).getMonth()!==new Date(this.props.time_start).getMonth() &&
+                            new Date(this.props.time_stop).getFullYear()!==new Date(this.props.time_start).getFullYear() ) &&
+                                new Date(this.props.time_stop).getDate() + ' ' + 
+                                this.getMonthName( new Date(this.props.time_stop).getMonth() ) + ' ' + 
+                                new Date(this.props.time_stop).getFullYear() + ', '
+                        }
+
+                        { //show no matter what
+                            new Date(this.props.time_stop).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+                        }
+                    </span>
+                }
+            </div>
+        );
+    }
+}
+
+export default DateTimeFromTo;
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import * as Constants from './constants';
+import DateTimeFromTo from './DateTimeFromTo';
 
 class SearchResults extends React.Component {
     constructor(props) {
         super(props);
-
-        this.getMonthName = this.getMonthName.bind(this);
-    }
-
-    getMonthName(m) {
-        return Constants.MONTH_NAME[m];
     }
 
     render () {
-
-
         return (
             <div>
                 { this.props.events &&
@@ -25,25 +17,7 @@ class SearchResults extends React.Component {
                                 { event.name } 
                                 <br />
 
-                                {                                    
-                                    new Date(event.time_start).getDate() + ' ' + 
-                                    this.getMonthName( new Date(event.time_start).getMonth() ) + ' ' + 
-                                    new Date(event.time_start).getFullYear() + ', ' + 
-                                    new Date(event.time_start).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' - ' 
-                                }
-
-                                { ( new Date(event.time_stop)!==new Date(event.time_start) && 
-                                    new Date(event.time_stop).getMonth()!==new Date(event.time_start).getMonth() &&
-                                    new Date(event.time_stop).getFullYear()!==new Date(event.time_start).getFullYear()
-                                  ) &&
-                                    new Date(event.time_stop).getDate() + ' ' + 
-                                    this.getMonthName( new Date(event.time_stop).getMonth() ) + ' ' + 
-                                    new Date(event.time_stop).getFullYear() + ', '
-                                }
-
-                                {
-                                    new Date(event.time_stop).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
-                                }
+                                <DateTimeFromTo time_start={event.time_start} time_stop={event.time_stop} />
                             </Link>
                         </h3>
                     )
