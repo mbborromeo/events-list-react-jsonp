3fadf8175ea22f7c7474f3a33966a789af4d34fd
Michael Borromeo <mbborromeo@gmail.com>
2019-10-06 17:03:39 -0700

    month name on List page

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as Constants from './constants';
 
 class SearchResults extends React.Component {
     constructor(props) {
         super(props);
 
+        this.getMonthName = this.getMonthName.bind(this);
+    }
+
+    getMonthName(m) {
+        return Constants.MONTH_NAME[m];
     }
 
     render () {
@@ -19,25 +25,24 @@ class SearchResults extends React.Component {
                                 { event.name } 
                                 <br />
 
-                                {
-                                    ' (' + 
-                                    new Date(event.time_start).getDate() + '-' + 
-                                    (new Date(event.time_start).getMonth()+1) + '-' + 
+                                {                                    
+                                    new Date(event.time_start).getDate() + ' ' + 
+                                    this.getMonthName( new Date(event.time_start).getMonth() ) + ' ' + 
                                     new Date(event.time_start).getFullYear() + ', ' + 
                                     new Date(event.time_start).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ' - ' 
                                 }
 
                                 { ( new Date(event.time_stop)!==new Date(event.time_start) && 
-                                    (new Date(event.time_stop).getMonth()+1)!==(new Date(event.time_start).getMonth()+1) &&
+                                    new Date(event.time_stop).getMonth()!==new Date(event.time_start).getMonth() &&
                                     new Date(event.time_stop).getFullYear()!==new Date(event.time_start).getFullYear()
                                   ) &&
-                                    new Date(event.time_stop).getDate() + '-' + 
-                                    (new Date(event.time_stop).getMonth()+1) + '-' + 
+                                    new Date(event.time_stop).getDate() + ' ' + 
+                                    this.getMonthName( new Date(event.time_stop).getMonth() ) + ' ' + 
                                     new Date(event.time_stop).getFullYear() + ', '
                                 }
 
                                 {
-                                    new Date(event.time_stop).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + ')' 
+                                    new Date(event.time_stop).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
                                 }
                             </Link>
                         </h3>
